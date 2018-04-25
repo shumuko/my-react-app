1ee94f46d18afcd4fe2a0648e647288a3911c60c
Fion Kou <fion@vermonster.com>
2018-04-25 12:20:15 -0400

    Add simple index.html, index.css, index.js to start with

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+var destination = document.querySelector("#js-main");
+
+ReactDOM.render(
+  <div>
+    <p>Hello</p>
+  </div>,
+  destination
+);
